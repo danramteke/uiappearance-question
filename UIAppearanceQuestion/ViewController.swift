4066d3ffe227e4f4b7b77b3eaab5a195d67f1580
Daniel Ramteke <danramteke@gmail.com>
2016-07-19 18:25:09 -0400

    Showing a hard to read alert box

--- UIAppearanceQuestion/ViewController.swift
+++ UIAppearanceQuestion/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "This app demonstrates some complaints about UIAppearance."
+        label.numberOfLines = 0
+        label.lineBreakMode = .ByWordWrapping
+        label.textAlignment = .Center
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Title"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Alert", style: .Plain, target: self, action: #selector(ViewController.showAlert))
+        
+        self.view.backgroundColor = .whiteColor()
+        self.view.addSubview(label)
+        
+        label.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor).active = true
+        label.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
+        label.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor, multiplier: 0.8).active = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func showAlert() {
+        let alert = UIAlertController(title: "Alert!", message: "An Alert Message!", preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK!", style: .Default, handler: { action in }))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
+    
 
 
 }
